a037d5ecfef7ebc484d949e8ecab5f5bd4ebfd12
Athenkosi Vinqi <nervadevelop@gmail.com>
2022-01-29 12:33:07 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navcompo from './components/Navcompo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-We goign Athi        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Navcompo />
+    </>
   );
 }
 
---
+++ src/components/Navcompo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navcompo = () => {
+  return (
+      <div>
+          <h1>Yoo New Changes</h1>
+      </div>
+  )
+};
+
+export default Navcompo;
